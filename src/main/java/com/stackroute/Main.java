5a361a4e0175b860655145cdbda9f5a0b5fe0e56
mynk009 <mayank.r@cgi.com>
2019-09-26 15:55:46 +0530

    first annot

---
+++ src/main/java/com/stackroute/Main.java
@@ -0,0 +1,16 @@
+package com.stackroute;
+
+import com.stackroute.domain.Movie;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+
+public class Main
+{
+    public static void main( String[] args )
+    {
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        Movie M1 = context.getBean("movie", Movie.class);
+        M1.role();
+    }
+}
---
+++ src/test/java/com/stackroute/MainTest.java
@@ -0,0 +1,16 @@
+package com.stackroute;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+
+public class MainTest
+{
+
+    @Test
+    public void shouldAnswerTrue()
+    {
+        assertTrue( true );
+    }
+}
